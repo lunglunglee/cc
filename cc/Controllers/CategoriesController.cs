766544c30f87713c6893d44cec43a4b626f16af9
donlee <lunglungleelee@hotmail.com>
2016-06-16 11:10:00 +0800

    g

---
+++ cc/Controllers/CategoriesController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using cc.Models;
+
+namespace cc.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private Model_cc db = new Model_cc();
+
+        // GET: Categories
+        public async Task<ActionResult> Index()
+        {
+            var categories = db.Categories.Include(c => c.Supplier);
+            return View(await categories.ToListAsync());
+        }
+
+        // GET: Categories/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Category category = await db.Categories.FindAsync(id);
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+            return View(category);
+        }
+
+        // GET: Categories/Create
+        public ActionResult Create()
+        {
+            ViewBag.貨源 = new SelectList(db.Suppliers, "SupplierID", "產品名稱");
+            return View();
+        }
+
+        // POST: Categories/Create
+        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
+        // 詳細資訊，請參閱 http://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "CategoryID,貨品名稱,QuantityPerUnit,Description,UnitPrice,UnitsInStock,缺貨,Picture,貨源,備註")] Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Categories.Add(category);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.貨源 = new SelectList(db.Suppliers, "SupplierID", "產品名稱", category.貨源);
+            return View(category);
+        }
+
+        // GET: Categories/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Category category = await db.Categories.FindAsync(id);
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.貨源 = new SelectList(db.Suppliers, "SupplierID", "產品名稱", category.貨源);
+            return View(category);
+        }
+
+        // POST: Categories/Edit/5
+        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
+        // 詳細資訊，請參閱 http://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "CategoryID,貨品名稱,QuantityPerUnit,Description,UnitPrice,UnitsInStock,缺貨,Picture,貨源,備註")] Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(category).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewBag.貨源 = new SelectList(db.Suppliers, "SupplierID", "產品名稱", category.貨源);
+            return View(category);
+        }
+
+        // GET: Categories/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Category category = await db.Categories.FindAsync(id);
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+            return View(category);
+        }
+
+        // POST: Categories/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Category category = await db.Categories.FindAsync(id);
+            db.Categories.Remove(category);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ cc/Controllers/代理表Controller.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using cc.Models;
+
+namespace cc.Controllers
+{
+    public class 代理表Controller : Controller
+    {
+        private Model_cc db = new Model_cc();
+
+        // GET: 代理表
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.代理表.ToListAsync());
+        }
+
+        // GET: 代理表/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            代理表 代理表 = await db.代理表.FindAsync(id);
+            if (代理表 == null)
+            {
+                return HttpNotFound();
+            }
+            return View(代理表);
+        }
+
+        // GET: 代理表/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: 代理表/Create
+        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
+        // 詳細資訊，請參閱 http://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "EmployeeID,代理稱謂,FirstName,Title,TitleOfCourtesy,BirthDate,HireDate,Address,City,Region,PostalCode,Country,HomePhone,Extension,Photo,Notes,ReportsTo,PhotoPath")] 代理表 代理表)
+        {
+            if (ModelState.IsValid)
+            {
+                db.代理表.Add(代理表);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(代理表);
+        }
+
+        // GET: 代理表/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            代理表 代理表 = await db.代理表.FindAsync(id);
+            if (代理表 == null)
+            {
+                return HttpNotFound();
+            }
+            return View(代理表);
+        }
+
+        // POST: 代理表/Edit/5
+        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
+        // 詳細資訊，請參閱 http://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "EmployeeID,代理稱謂,FirstName,Title,TitleOfCourtesy,BirthDate,HireDate,Address,City,Region,PostalCode,Country,HomePhone,Extension,Photo,Notes,ReportsTo,PhotoPath")] 代理表 代理表)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(代理表).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(代理表);
+        }
+
+        // GET: 代理表/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            代理表 代理表 = await db.代理表.FindAsync(id);
+            if (代理表 == null)
+            {
+                return HttpNotFound();
+            }
+            return View(代理表);
+        }
+
+        // POST: 代理表/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            代理表 代理表 = await db.代理表.FindAsync(id);
+            db.代理表.Remove(代理表);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
